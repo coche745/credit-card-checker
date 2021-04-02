98dffc03f3cde60fcd311e8a0b7be3649dc50e79
Coche <70551980+coche745@users.noreply.github.com>
2021-04-02 16:23:12 -0500

    explanations

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
-// More invalid card numbers as strings
+// More valid card numbers as strings
 let validString1 = '4929913913793687'
 let validString2 = '4556461495038097'
 let validString3 = '4485132499775270147'
@@ -36,7 +36,7 @@ let validString10 = '379169196749606'
 let validString11 = '370606425416183'
 let validString12 = '341837331325004'
 
-// list of invalid strings
+// list of valid strings
 
 const stringBatch = [validString1, validString2, validString3, validString4, validString5, validString6, validString7, validString8, validString9, validString10, validString11, validString12];
 
@@ -54,7 +54,6 @@ const validateCred = (arr) => {
         } else {
             newArr.unshift(arr[i]);
         }
-        
     }
     let total = newArr.reduce((a, b) => a + b);
     if (total % 10 === 0) {
@@ -127,22 +126,39 @@ const invalidToValid = (arr) => {
     return arr;
 }
 
-const validBatch = stringBatch.map(stringToArr);
+console.log('\nCredit card numbers are valid if they pass a test done with the Luhn algorithm, which is implemented in our code.');
+console.log('\nCheck if credit card number is valid.');
+console.log(valid1);
+console.log(validateCred(valid1));
+console.log(invalid1);
+console.log(validateCred(invalid1));
+
 
 const invalidArr = findInvalidCards(batch);
 
+console.log('\nFind invalid numbers out of list of 15 credit card numbers.');
+console.log('Here are the invalid numbers below.')
 console.log(invalidArr);
 
+console.log('\nOut of the invalid numbers above, these are the credit card companies they were issued from');
 console.log(idInvalidCardCompanies(invalidArr));
 
+console.log('\nUsing invalidToValid function, turn two invalid card numbers into valid ones');
+console.log('Test to make sure they are now valid');
 const nowValid1 = invalidToValid(invalid1);
-
 const nowValid2 = invalidToValid(invalid2);
-
 console.log(validateCred(nowValid1));
-
 console.log(validateCred(nowValid2));
 
+console.log('\nUsing an online credit card generator, I have taken 12 card numbers as strings');
+console.log('I will turn these strings into arrays in order to more easily check if they are valid');
+console.log('Here is the result below.');
+const validBatch = stringBatch.map(stringToArr);
+console.log(validBatch);
+
+console.log('\nNow I will see if there are any invalid numbers in this list. None should be returned.');
+const invalidArr2 = findInvalidCards(validBatch);
+console.log(invalidArr2);
 
 
 
